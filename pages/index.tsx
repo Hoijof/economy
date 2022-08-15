787268ba117b27dbd3726ef5ab84d76e236cf6d8
Humberto Gomez Lopez <hoijof@gmail.com>
2022-08-15 16:58:10 +0200

    feat: add mui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,65 @@
-import React from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
 
-import createDb from '../utils/db';
+import { Typography, Grid, Button } from "@mui/material";
+
+import createDb from "../utils/db";
 
 export default function Home() {
-  const [oMoney, setOMoney]:  [any, any] = React.useState({});
-  const [money, setMoney] = React.useState(0);
-  const [db, setDb] = React.useState(null);
+    const [oMoney, setOMoney]: [any, any] = React.useState({});
+    const [money, setMoney] = React.useState(0);
+    const [db, setDb] = React.useState(null);
 
-  React.useEffect(() => {
-    if (!db) {
-      setDb(createDb('money'));
+    React.useEffect(() => {
+        if (!db) {
+            setDb(createDb("money"));
 
-      return;
-    }
+            return;
+        }
 
-    let dMoney = db.getC('money', 1);
+        let dMoney = db.getC("money", 1);
 
-    if (!dMoney) {
-      dMoney = db.add('money', { cash: 0 });
-    }
+        if (!dMoney) {
+            dMoney = db.add("money", { cash: 0 });
+        }
 
-    setOMoney(dMoney);
-    setMoney(dMoney.cash);
+        setOMoney(dMoney);
+        setMoney(dMoney.cash);
+    }, [db]);
 
-  } ,[db]);
+    const addMoney = React.useCallback(() => {
+        oMoney.cash = oMoney.cash + 1;
 
-  const addMoney = React.useCallback(() => {
-    oMoney.cash = oMoney.cash + 1;
+        db.update("money", oMoney.id, { cash: oMoney.cash });
 
-    db.update('money', oMoney.id, { cash: oMoney.cash});
+        setMoney(oMoney.cash);
+    }, [db, oMoney]);
 
-    setMoney(oMoney.cash);
-  } , [db, oMoney]);
+    const removeMoney = React.useCallback(() => {
+        oMoney.cash = oMoney.cash - 1;
 
-  const removeMoney = React.useCallback(() => {
-    oMoney.cash = oMoney.cash - 1;
+        db.update("money", oMoney.id, { cash: oMoney.cash });
 
-    db.update('money', oMoney.id, { cash: oMoney.cash});
+        setMoney(oMoney.cash);
+    }, [db, oMoney]);
 
-    setMoney(oMoney.cash);
-  } , [db, oMoney]);
+    return (
+        <div>
+            <Head>
+                <title>Summary</title>
+                <meta name="description" content="General summary of Economy" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Summary</title>
-        <meta name="description" content="General summary of Economy" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+            <main>
+                <Typography variant="h4">Summary</Typography>
+                <Typography variant="body1">This is the summary of the economy.</Typography>
+                <Typography variant="body1"> Your money: {money}</Typography>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Summary
-        </h1>
-        <p className={styles.description}>
-          This is the summary of the economy.
-        </p>
-        <p> Your money: {money}</p>
-
-        <button onClick={addMoney}>Add Money</button>
-        <button onClick={removeMoney}>Remove Money</button>
-      </main>
-    </div>
-  );
-  
+                <Button onClick={addMoney}>Add Money</Button>
+                <Button onClick={removeMoney}>Remove Money</Button>
+            </main>
+        </div>
+    );
 }
